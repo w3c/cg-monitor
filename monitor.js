363c7071910e75592c82943b62125e874a8dfc2b
Dominique Hazael-Massieux <dom@w3.org>
2018-12-05 17:13:35 +0100

    Scrape mailing list info from HTML and add network request queuing

--- monitor.js
+++ monitor.js
@@ -1,13 +1,36 @@
 const url = require("url");
-const fetch = require("node-fetch");
+const _fetch = require("node-fetch");
+const jsdom = require("jsdom");
+const RequestQueue = require('limited-request-queue');
+
 //const Octokat = require("octokat");
 const config = require("./config.json");
 const w3c = require('node-w3capi');
 let RSSParser = require('rss-parser');
+const { JSDOM } = jsdom;
 const rssparser = new RSSParser();
 const MWBot = require('nodemw');
 
 w3c.apiKey = config.w3capikey;
+
+const fetchResolve = {};
+const fetchReject = {};
+
+const queue = new RequestQueue(null, {
+  'item': ({url}, done) => {
+    console.warn("fetching " + url);
+    _fetch(url).then(r => {
+      done();
+      return fetchResolve[url](r);
+    }).catch(fetchReject[url]);
+  }
+});
+
+const fetch = url => new Promise((res, rej) => {
+  fetchResolve[url] = res;
+  fetchReject[url] = rej;
+  queue.enqueue(url);
+});
 
 const _p = pathObj => function() {
   const args = [...arguments];
@@ -25,19 +48,36 @@ const httpToHttps = str => str.replace(/^http:\/\//, "https://");
 const relevantServices = ["rss", "lists", "repository", "wiki"];
 
 function fetchRSS(url) {
-  return rssparser.parseURL(url).catch(error => "Error fetching "  + url + ": " + error);
+  return fetch(url).then(r => r.text()).then(text => rssparser.parseString(text)).catch(error => "Error fetching "  + url + ": " + error);
 }
 
 function fetchMail(url) {
   if (!httpToHttps(url).startsWith('https://lists.w3.org/Archives/Public')) return Promise.resolve("Did not fetch " + url);
-  return fetchRSS(url + 'feed.rss');
+  return fetch(url)
+    .then(r => r.text())
+    .then(text => new JSDOM(text))
+    .then(dom => {
+      const data = {};
+      [...dom.window.document.querySelectorAll("tbody")].forEach(tbody => {
+        [...tbody.querySelectorAll("tr")].forEach(tr => {
+          const month = new Date(tr.querySelector("td").textContent + " GMT");
+          if (month.toJSON())
+            data[month.toJSON().slice(0,7)] = parseInt(tr.querySelector("td:last-child").textContent, 10);
+          else
+            console.error("Error parsing ml archive at " + url);
+        });
+      });
+      return data;
+    });
 }
 
 function fetchWiki(url) {
-  return fetchRSS(url + '/api.php?action=feedrecentchanges');
+  if (!url.startsWith('http')) url = 'https://www.w3.org' + url;
+  return fetchRSS(url + '/api.php?action=feedrecentchanges&from=' + 1514761200);
 }
 
 function fetchGithub(url) {
+  // Repo vs Org
   return fetchRSS(url + 'commits/master.atom'); // TODO: figure the right branch
   // TODO: detect comment / issue / PR activity as well
 }
