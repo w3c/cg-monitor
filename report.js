77782f0ba3a61bd1c825843eae4e777a1127a9dd
Dominique Hazael-Massieux <dom@w3.org>
2018-12-06 08:42:33 +0100

    Make report more compact

--- report.js
+++ report.js
@@ -21,100 +21,66 @@ const lastTwelveMonths = (() => {
   return months;
 })();
 
+const bar = (count, fill) => {
+  const width = count ? 4*Math.log(count, 2) : 0;
+  return `<svg width=${Math.max(width, 48)} height='16' viewBox='0 0 ${Math.max(width, 30)} 16'><rect x='0' y='0' height='16' width='${width}' fill='${fill}'/><text y='15'>${count ? count : ''}</text></svg>`;
+};
+
 fetch("report.json").then(r => r.json())
   .then(groupdata => {
     groupdata.forEach(d => {
-      const section = document.createElement("section");
+      const section = document.createElement("tr");
 
-      const h2 = document.createElement("h2");
+      const h2 = document.createElement("th");
       const link = document.createElement("a");
-      link.appendChild(document.createTextNode(d[0].name));
+      link.appendChild(document.createTextNode(d[0].name.replace(/ Community Group/, '')));
       link.href = d[0]._links.homepage.href;
       h2.appendChild(link);
       section.appendChild(h2);
 
 
-      const chairs = document.createElement("p");
+      if (d[2] && d[2].length) {
+        // aggregate by service type
+        const serviceData = d[2].reduce((acc, {service, data}) => {
+          const existingService = acc.find(s => s.service.type === service.type);
+          if (existingService) {
+            if (existingService.data.items) existingService.data.items = existingService.data.items.concat(data.items);
+            else existingService.data = [...new Set(Object.keys(existingService.data).concat(Object.keys(data)))]
+              .reduce((acc, m) => {
+                acc[m] = (existingService.data[m] || 0) + (data[m] || 0);
+                return acc;
+            }, {});
+          } else {
+            acc.push({service, data});
+          }
+          return acc;
+        }, []);
+        ['lists', 'repository', 'wiki', 'rss', 'participation']
+          .forEach(servicetype => {
+            const activity = document.createElement("td");
+            const data = (serviceData.find(s => s.service.type === servicetype) || {}).data;
+            let val;
+            if (data) {
+              if (data.items) {
+                val = lastTwelveMonths.reduce((acc, m) => acc + data.items.filter(i => (i.isoDate && i.isoDate.startsWith(m)) || (i.created_at && i.created_at.startsWith(m))).length, 0);
+              } else {
+                val = lastTwelveMonths.reduce((acc, m) => acc + (data[m] || 0), 0);
+              }
+              activity.innerHTML = bar(val, colors[servicetype]);
+            }
+            section.appendChild(activity);
+          });
+      }
+      const chairs = document.createElement("td");
       if (d[1] && d[1].length) {
         chairs.classList.add("yes");
         chairs.appendChild(document.createTextNode(d[1].length + " chairs"));
       } else {
         chairs.classList.add("no chairs");
         chairs.appendChild(document.createTextNode("no"));
-
       }
       section.appendChild(chairs);
 
-      const activity = document.createElement("section");
-      if (d[2] && d[2].length) {
-        const chart = document.createElement("div");
-        const columns = [['x'].concat(lastTwelveMonths)];
-        const groups = [];
-        d[2].forEach(({service, data}) => {
-          if (data.items) {
-            const col = [service.type];
-            groups.push(service.type);
-            lastTwelveMonths.forEach(m => {
-              col.push(data.items.filter(i => i.isoDate.startsWith(m)).length);
-            });
-            columns.push(col);
-          } else {
-            const col = [service.type];
-            groups.push(service.type);
-            lastTwelveMonths.forEach(m => {
-              col.push(data[m] || 0);
-            });
-            columns.push(col);
-          }
-        });
-        const aggregatedColumns = columns.reduce((acc, col) => {
-
-          const existingCol = acc.find(c => c[0] === col[0]);
-          if (existingCol) {
-            existingCol.forEach((x,i) => { if (i > 0) x += col[i] });
-          } else {
-            acc.push(col);
-          }
-          return acc;
-        }, []);
-        const values = aggregatedColumns.slice(1).reduce((acc, col) => acc.concat(col.slice(1)), []);
-        c3.generate({
-          bindto: chart,
-          data : {
-            x: 'x',
-            xFormat: '%Y-%m',
-            type: 'bar',
-            columns: aggregatedColumns,
-            groups: [groups]
-          },
-          axis: {
-            x: {
-              type: 'timeseries',
-              tick: {
-                format: '%Y-%m-%d'
-              }
-            },
-            y: {
-              min: 0,
-              padding: { bottom: 0 },
-              max: Math.max.apply(null, [10].concat(values)),
-              tick: {
-                format: d3.format('d')
-              }
-            }
-          },
-          color: {
-            pattern: aggregatedColumns.slice(1).map(c => colors[c[0]] || '#000')
-          }
-        });
-        activity.appendChild(chart);
-      } else {
-        activity.classList.add("no");
-        activity.appendChild(document.createTextNode("no identified service"));
-      }
-      section.appendChild(activity);
-
-      
       main.appendChild(section);
     });
   });
