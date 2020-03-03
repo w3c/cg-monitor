51f4858102f713d58a39dd9d449d2877a2e75f2a
Dominique Hazael-Massieux <dom@w3.org>
2020-03-03 14:40:06 +0100

    Handle mailing list archives with quarterly grouping

--- monitor.js
+++ monitor.js
@@ -64,10 +64,23 @@ function fetchMail(url) {
       [...dom.window.document.querySelectorAll("tbody")].forEach(tbody => {
         [...tbody.querySelectorAll("tr")].forEach(tr => {
           const month = new Date(tr.querySelector("td").textContent + " GMT");
-          if (month.toJSON())
-            data[month.toJSON().slice(0,7)] = parseInt(tr.querySelector("td:last-child").textContent, 10);
-          else
+          if (month.toJSON()) {
+            const mailCount = parseInt(tr.querySelector("td:last-child").textContent, 10);;
+            // some archives are per quarter
+            // we detect this on the presence of the string " to "
+            // as in "January to March"
+            if (tr.querySelector("td").textContent.includes(" to ")) {
+              // and if so, we divide arbitrarily in 3 for the per-month view
+              for (let i = 0; i < 3 ; i++) {
+                data[month.toJSON().slice(0,7)] = mailCount / 3;
+                month.setMonth(month.getMonth() - 1);
+              }
+            } else {
+              data[month.toJSON().slice(0,7)] = mailCount;
+            }
+          } else {
             console.error("Error parsing ml archive at " + url);
+          }
         });
       });
       return data;
