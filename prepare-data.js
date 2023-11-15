9b4bc3bb9bba31028de76fad63621d6cc239b081
Dominique Hazael-Massieux <dom@w3.org>
2023-11-15 08:23:01 +0100

    Improve readability of prepare-data

--- prepare-data.js
+++ prepare-data.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 const { computeGroupData } = require("./lib/compute-group-data");
 
 const target = "./report.json";
@@ -6,21 +6,30 @@ const target = "./report.json";
 const aggregatedData = [];
 
 const loadDir = async (dirPath) => {
-  const files = await fs.promises.readdir(dirPath);
-  return fs.promises.readFile(dirPath + '/staff.json', 'utf-8')
-    .then(JSON.parse)
-    .then(staff => Promise.all(
-      files.filter(path => path.match(/\.json$/)).filter(path => path !== 'staff.json')
-        .map(
-          path => fs.promises.readFile(dirPath + "/" + path, 'utf-8')
-            .then(JSON.parse)
-            .catch(err => { console.error("Failed parsing " + path + ": " + err);})
-            .then(data => computeGroupData(data, staff)).catch(e => console.error("Error while dealing with " + path + ":" + JSON.stringify(e.stack)))
-        )
-    ));
+  const files = (await fs.readdir(dirPath)).filter(path => path.match(/\.json$/) && path !== 'staff.json');
+  const staff = JSON.parse(await fs.readFile(dirPath + '/staff.json', 'utf-8'));
+  return Promise.all(
+    files
+      .map(
+        async (path) => {
+	  let data;
+	  try {
+	    data = JSON.parse(await fs.readFile(dirPath + "/" + path, 'utf-8'));
+	  } catch (err) {
+	    console.error("Failed parsing " + path + ": " + err);
+	    return;
+	  }
+	  try {
+	    return computeGroupData(data, staff);
+	  } catch (e) {
+	    console.error("Error while dealing with " + path + ":" + JSON.stringify(e.stack));
+	  }
+	}
+      )
+  );
 };
 
-loadDir("./data").then(data => {
-  fs.writeFileSync(target, JSON.stringify({timestamp: new Date(), data: data.filter(x => x)}, null, 2));
-});
+loadDir("./data").then(data =>
+  fs.writeFile(target, JSON.stringify({timestamp: new Date(), data: data.filter(x => x)}, null, 2))
+);
 
